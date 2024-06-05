b4244a644648f33036ec1ac5731f6385479c27fa
z4bulon <z8bulon@gmail.com>
2024-06-05 20:45:30 +0500

    Changed app, authorization validation and user controllers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require('path');
 const userController = require("../controller/userController.js");
 const auth = require('../middleware/auth');
 const upload = require('../middleware/upload');
